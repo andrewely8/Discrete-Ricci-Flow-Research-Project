212fa57831460116cc9dce66d88569f6aac13106
andrewely8 <andrewely08@gmail.com>
2026-02-09 04:05:45 -0500

    constructed data sets

--- community detection algorithms/test.py
+++ community detection algorithms/test.py
@@ -3,12 +3,10 @@ import graphClass
 import graphNetworks
 import networkx as nx
 import ot
+import sys
+import os
 
-myGraph = graphClass.CurvatureGraph(graphNetworks.zacharyKarateClub)
-G = myGraph.drawGraph(display=False)
-comms = nx.community.louvain_communities(G,resolution=0.5)
-groundTruth = [{0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 16, 17, 19, 21}, {9, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,33}]
-colorMap = {}
+myGraph = graphClass.CurvatureGraph(graphNetworks.euEmailCore)
 
 colors = [
 			(255, 0, 0), (0, 0, 255), (0, 0, 0), (0, 255, 0), (1, 255, 254), (255, 166, 254), (255, 219, 102), (0, 100, 1), (1, 0, 103), 
@@ -21,13 +19,27 @@ colors = [
 			(107, 104, 130), (95, 173, 78), (167, 87, 64), (165, 255, 210), (255, 177, 103), (0, 155, 255), (232, 94, 190)
 		]
 
-for node in myGraph.nodes:
-	if node in groundTruth[0]:
-		colorMap[node] = '255,0,0'
-	else:
-		colorMap[node] = '0,0,255'
+colorMap = {}
 
-myGraph.drawGraph(display=True, savePath = 'test.gexf',colorMap=colorMap)
+cur_path = os.path.dirname(__file__)
+file_path = os.path.relpath('..\\graph creation tools\\email-Eu-core/email-Eu-core-department-labels.txt', cur_path)
+comms = []
+
+with open(file_path, 'r') as f:
+	for line in f:
+		line = line.strip()
+		line = line.split(' ')
+		node = int(line[0])
+		community = int(line[1])
+		colorAssignIndex = community % len(colors)
+		colorAssingTuple = colors[colorAssignIndex]
+		color = f"{colorAssingTuple[0]},{colorAssingTuple[1]},{colorAssingTuple[2]}"
+		colorMap[node] = color
+
+G = myGraph.drawGraph(display=False)
+comms = nx.community.louvain_communities(G,resolution=0.5)
+# groundTruth = [{0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 16, 17, 19, 21}, {9, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,33}]
+
 
 toDelete = []
 
@@ -43,4 +55,4 @@ for edge in myGraph.edges:
 for edge in toDelete:
 	myGraph.removeEdge(edge)
 
-
+myGraph.drawGraph(display=False, savePath = 'test.gexf',colorMap=colorMap)
